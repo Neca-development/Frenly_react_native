9af2710b105005266d0c01fddd91dae3d3acfd93
dmitry-sharashkin <faustfbs@mail.ru>
2022-11-15 11:27:14 +0600

    feat: skeleton & comment data

--- metro.config.js
+++ metro.config.js
@@ -11,7 +11,7 @@ config.resolver = {
     fs: require.resolve("expo-file-system"),
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
-    // net: require.resolve('react-native-tcp'),
+    net: require.resolve("react-native-tcp-socket"),
     os: require.resolve("os-browserify/browser.js"),
     path: require.resolve("path-browserify"),
     stream: require.resolve("readable-stream"),
